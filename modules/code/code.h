0e8d5a255c0b91c3a0e9cca85f03bac50a4bd8d5
lillymarletta <155563354+lillymarletta@users.noreply.github.com>
2024-03-05 12:43:13 -0500

    Update code.h

--- modules/code/code.h
+++ modules/code/code.h
@@ -5,11 +5,13 @@
 
 //=====[Declaration of public defines]=========================================
 
+#define TIMER_MAX_KEYS = 4
 
 //=====[Declaration of public data types]======================================
 
 //=====[Declarations (prototypes) of public functions]=========================
 
+void timerWrite( char* timerInput );
 
 //=====[#include guards - end]=================================================
 
